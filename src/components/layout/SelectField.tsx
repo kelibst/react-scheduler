2a99817cb9d2b98cbaaace1e280ee0e55087f4bc
kelibst <kbooster17@gmail.com>
2023-03-12 22:41:33 +0000

    add month and week view select

---
+++ src/components/layout/SelectField.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { setweekView } from '../../redux/reducers/notificationReducer';
+
+interface SelectProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+const SelectField: React.FC<SelectProps> = ({ value, onChange }) => {
+  const [selectedValue, setSelectedValue] = useState(value);
+  const dispatch = useDispatch()
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value;
+    setSelectedValue(value);
+    dispatch(setweekView())
+    onChange(value);
+  };
+
+  return (
+    <select className='border-0 cursor-pointer outline-none' value={selectedValue} onChange={handleOnChange}>
+      <option value="Week">Week</option>
+      <option value="Month">Month</option>
+    </select>
+  );
+};
+
+export default SelectField;
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,6 +1,9 @@
 import { FC, useState } from 'react';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../redux/store';
 import UsersList from '../UsersList';
+import SelectField from './SelectField';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -8,6 +11,11 @@ interface LayoutProps {
 
 const Layout: FC<LayoutProps> = ({ children }) => {
   const [sidebarOpen, setSidebarOpen] = useState(true);
+  const [selectedOption, setSelectedOption] = useState('Week');
+  const { years, activeWeek, activeMonth } = useSelector((state: RootState) => state.month)
+  const handleSelectChange = (value: string) => {
+    setSelectedOption(value);
+  };
 
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
@@ -17,9 +25,8 @@ const Layout: FC<LayoutProps> = ({ children }) => {
     <div className="flex min-h-screen">
       {/* Sidebar */}
       <div
-        className={`fixed shadow-md h-full bg-gray-100 text-black w-64 p-4 transition-all duration-300 ease-in-out ${
-          sidebarOpen ? 'left-0' : '-left-64'
-        }`}
+        className={`fixed shadow-md h-full bg-gray-100 text-black w-64 p-4 transition-all duration-300 ease-in-out ${sidebarOpen ? 'left-0' : '-left-64'
+          }`}
       >
         <div className="flex justify-between items-center mb-8">
           <h2 className="text-lg font-bold">Klight Solutions</h2>
@@ -43,7 +50,7 @@ const Layout: FC<LayoutProps> = ({ children }) => {
       <div className={`flex-1 flex flex-col relative w-full ${!sidebarOpen ? 'ml-0' : 'ml-64'}`}>
         {/* Top bar */}
         <div className="w-full flex bg-white shadow-md text-dark p-4">
-          <button className={`relative shadow-md top-8 bg-white px-4 mx-3 rounded-2xl ${sidebarOpen ? "right-14": "right-4"}`} onClick={toggleSidebar}>
+          <button className={`relative shadow-md top-8 bg-white px-4 mx-3 rounded-2xl ${sidebarOpen ? "right-14" : "right-4"}`} onClick={toggleSidebar}>
             {sidebarOpen ? (
               // <FaChevronLeft className="text-gray-400" />
               <div className='flex justify-center items-center'><FaChevronLeft /> <span className="ml-2">Hide</span> </div>
@@ -54,7 +61,15 @@ const Layout: FC<LayoutProps> = ({ children }) => {
           </button>
           <h2 className="text-lg font-bold">Duty Roaster Application</h2>
         </div>
-
+        <div className='flex justify-between md:px-4 md:py-8 shadow-md'>
+          <div>
+            <span className="font-bold">Schedule : <span className="text-blue-400"> <SelectField value={selectedOption} onChange={handleSelectChange} /></span></span> 
+          </div>
+              <div>
+              {activeWeek.format('MMM')} - {activeWeek.format("YY")}
+              </div>
+              <div>actions</div>
+        </div>
         {/* Content */}
         <div className={`flex-1 p-4 ${!sidebarOpen ? 'w-full' : ''}`}>{children}</div>
       </div>
